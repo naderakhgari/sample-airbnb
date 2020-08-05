d1d6157a5ee54a89ae3a9ff9df7282578105af90
Nader Akhgari <nader90akhgari@gmail.com>
2020-08-06 00:59:28 +0100

    Done the endpoint which is search for room by price

--- server.js
+++ server.js
@@ -13,12 +13,27 @@ client.connect(() => {
   const db = client.db("sample_airbnb");
   const collection = db.collection("listingsAndReviews");
 
-  app.get("/search", (req, res) => {
+  app.get("/room/search", (req, res) => {
     const { name, summary } = req.query;
     if (!name && !summary) {
       return res.status(400).send("please try again with name or summary!");
     }
     const searchObj = { $or: [{ name: name }, { summary: summary }] };
+    collection.find(searchObj).toArray((err, result) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(result);
+      }
+    });
+  });
+
+  app.get("/room/:price", (req, res) => {
+    if (req.params.price === undefined) {
+        return res.status(400).send("please try again with name or summary!");
+      }
+    const { price } = req.params;
+    const searchObj = { price:Number(price) };
     collection.find(searchObj).toArray((err, result) => {
       if (err) {
         res.status(500).send(err);
